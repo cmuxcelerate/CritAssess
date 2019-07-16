e709f4bd6b0b436dd3bddadfc225868f622838cb
Ryan Emberling <itsalaidbacklife@gmail.com>
2019-07-16 10:54:12 -0400

    Added scenarios 4 and 5

--- src/app/assessment_data/assessment_classes/assessment.ts
+++ src/app/assessment_data/assessment_classes/assessment.ts
@@ -8,7 +8,7 @@ export class Assessment {
 	questionsAnswered: number; //How many questions have been answered so far
 	constructor(questionGroups: Question[][]) {
 		this.questionGroups = questionGroups;
-		this.scenario = 0;
+		this.scenario = 4;
 		this.question = 0;
 		this.confirmedAnswer = false;
 		this.questionsAnswered = 0;
