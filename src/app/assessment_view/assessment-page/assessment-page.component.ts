7c4de4ad6f70e7638d4fc1e11167ca4537867990
Ryan Emberling <itsalaidbacklife@gmail.com>
2019-07-26 14:58:56 -0400

    Responsive page heights for directions and assessment (prevents whitespace at page bottom for short pages

--- src/app/assessment_view/assessment-page/assessment-page.component.ts
+++ src/app/assessment_view/assessment-page/assessment-page.component.ts
@@ -8,13 +8,16 @@ import { AssessmentService } from '../../assessment_data/assessment.service';
 })
 export class AssessmentPageComponent implements OnInit {
 
-
+	minimumHeight: string; //Minimum height of this page (prevents whitespace at page bottom)
 	get assessment() {
 		return this.assessmentService.assessment;
 	}
 	constructor(private assessmentService: AssessmentService) { }
 
 	ngOnInit() {
+		// min height is window - header - assessmentHeader - footer
+		const height = window.innerHeight - 82 - 52 - 78;
+		this.minimumHeight =  height + "px";
 	}
 
 }
