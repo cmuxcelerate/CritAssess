f7e9846ce1dc5f9e215c1751bd0e876386a5f68c
Ryan Emberling <itsalaidbacklife@gmail.com>
2019-07-26 22:33:24 -0400

    sections

--- src/app/pre-test_pages/choose-section/choose-section.component.ts
+++ src/app/pre-test_pages/choose-section/choose-section.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-section.component.css']
 })
 export class ChooseSectionComponent implements OnInit {
+	minimumHeight: string; //Minimum height of this page (prevents whitespace at page bottom)
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+		// min height is window - header - footer
+		const height = window.innerHeight - 52 - 78;
+		this.minimumHeight =  height + "px";
+	}
 
 }
